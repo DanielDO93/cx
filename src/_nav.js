5b3950b73b7dbf930ed771d723d68508c385620b
DanielDO93 <danieldionisio1993@gmail.com>
2020-11-14 09:37:06 -0600

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -6,10 +6,170 @@ export default {
       class: 'text-center',
     },
     {
-      name: 'Reglas ortográficas de la V',
+      name: 'Objetivo',
+      icon: 'icon-tag',
+      url: "/temarioViews/ObjetivoView",
+    },
+    {
+      name: 'Índice',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemarioView",
+    },
+    {
+      name: 'Introducción',
+      icon: 'icon-tag',
+      url: "/temarioViews/IntroduccionView",
+     
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P1',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaDosView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P2',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaTresView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P3',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaCuatroView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P4',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaCincoView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P5',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaSeisView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P6',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaSieteView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P7',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaOchoView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P8',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaNueveView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P9',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaDiezView",
+    },
+    {
+      name: '1. ¿Qué significa experiencia al cliente? > P10 ',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaOnceView",
+    },
+    {
+      name: '2. ¿Por qué es importante para las empresas? > P1',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaTreceView",
+    },
+    {
+      name: '2. ¿Por qué es importante para las empresas? > P2',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaCatorceView",
+    },
+
+    {
+      name: '2. ¿Por qué es importante para las empresas? > P3',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaQuiceView",
+    },
+    {
+      name: '2. ¿Por qué es importante para las empresas? > P4',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaDieciseisView",
+    },
+
+    {
+      name: '3. Ventajas de una Experiencia al Cliente > P1',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaDieciochoView",
+    },
+    {
+      name: '3. Ventajas de una Experiencia al Cliente > P2',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaDiecinueveView",
+    },
+    {
+      name: '3. Ventajas de una Experiencia al Cliente > P3',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaVienteView",
+    },
+    {
+      name: '3. Ventajas de una Experiencia al Cliente > P4',
+      icon: 'icon-tag',
+      url: "/temarioViews/TemaVientiunoView",
+    },
+
+    {
+      name: '4. Factores que causan una mala experiencia al cliente > P1',
       icon: 'icon-tag',
      
     },
+    {
+      name: '4. Factores que causan una mala experiencia al cliente > P2',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '4. Factores que causan una mala experiencia al cliente  > P3',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P1',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P2',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P3',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P4',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P5',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P6',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P7',
+      icon: 'icon-tag',
+     
+    },
+    {
+      name: '5. Características de una estrategia de Experiencia al cliente > P8',
+      icon: 'icon-tag',
+     
+    },
+   
 
    
     {
@@ -22,23 +182,13 @@ export default {
       icon: 'icon-puzzle',
     
     },
-    {
-      name: 'Evaluación 3',
-      icon: 'icon-puzzle',
     
-    },
-
     {
-      name: 'Conclusión',
+      name: 'Evaluación del curso',
       icon: 'icon-tag',
-      
+     
     },
-
-    {
-      name: 'Despedida',
-      icon: 'icon-tag',
-      
-    },
+    
 
   ],
 }
