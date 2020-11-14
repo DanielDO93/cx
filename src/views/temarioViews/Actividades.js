70ffa3b7ce8501653db93e650654793024d79760
DanielDO93 <danieldionisio1993@gmail.com>
2020-11-13 19:06:46 -0600

    Update Actividades.js

--- src/views/temarioViews/Actividades.js
+++ src/views/temarioViews/Actividades.js
@@ -11,26 +11,28 @@ class Actividades extends Component {
     return (
       <div className="animated fadeIn ">
         <CardHeader className="text-left">
-        
+
         </CardHeader>
         <CardBody className="">
           <Row className="centrado-fila">
-           
+
             <Col xs="9">
               <Row className="centrado-fila">
                 <Col xs="12">
                   <div
-                  
+
                     className="cajaRoja
                    ml-2 mr-2"
                   >
-                    <h2 className="text-center">A continuación, realizaremos una evaluación para reforzar los conocimientos adquiridos.</h2>
+                    <h2 className="text-center">
+                      A continuación, realizaremos una evaluación para reforzar los conocimientos adquiridos.
+                    </h2>
                     <img
-                  src={logo2}
-                  style={{  }}
-                  alt="logo2 "
-                  className="img-fluid bordeImagen animated  bounceInLeft delay-1s"
-                />
+                      src={logo2}
+                      style={{}}
+                      alt="logo2 "
+                      className="img-fluid bordeImagen animated  bounceInLeft delay-1s"
+                    />
                   </div>
                 </Col>
               </Row>
